0ff0480b9d7f205f3300bbba2a0b39d79cd573a5
Dexter Ty <dexter.gets@gmail.com>
2023-10-26 08:41:13 -0400

    Custom JavaScript Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-console.log('javascript connected!');
+/* console.log('javascript connected!');
     
 const carousel = new bootstrap.Carousel('#homeCarousel', {
     interval: 2000,
@@ -17,4 +17,23 @@ const carouselPlay = document.getElementById('carouselPlay');
 carouselPlay.addEventListener('click', function() {
     console.log('cycle the carousel');
     carousel.cycle();
+}) */
+const carousel = new bootstrap.Carousel('#homeCarousel', {
+    interval: 2000,
+    pause: false
 })
+
+const carouselButton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
+
+carouselButton.addEventListener('click', function () {
+    if (faIcon.classList.contains('fa-pause')) {
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carousel.pause();
+    } else {
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carousel.cycle();
+    }
+}) 

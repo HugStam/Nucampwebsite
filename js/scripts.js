f239ddda652d2bef6216439017185360037f9f7e
Dexter Ty <dexter.gets@gmail.com>
2023-11-05 10:09:59 -0500

    final for WShop5

--- js/scripts.js
+++ js/scripts.js
@@ -37,3 +37,36 @@ carouselButton.addEventListener('click', function () {
         carousel.cycle();
     }
 }) 
+
+async function fetchWeather() {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    const city = 'Stamford';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+
+    try {
+        const response = await fetch(url)
+        console.log('from url from end point', response)
+        const data = await response.json()
+        console.log(data.weather[0].description)
+        console.log(data)
+        displayWeather(data)
+    }catch(error) {
+       // console.log(error)
+    }
+}
+
+function displayWeather(data) {
+    const temperature = data.main.temp
+    const weatherDescription = data.weather[0].description
+    const weatherIcon = data.weather[0].icon 
+    const imgElement = document.createElement('img');
+    imgElement.src = `https://openweathermap.org/img/w/${weatherIcon}.png`
+    imgElement.alt = 'weather icon';
+    const iconElement = document.getElementById('weather-icon');
+    iconElement.appendChild(imgElement);
+    const tempLink = document.getElementById('weather-temp');
+    tempLink.textContent = `${temperature} \u00B0f`;
+    const tempDesc = document.getElementById('weather-description');
+    tempDesc.textContent = weatherDescription;  
+}
+fetchWeather();
